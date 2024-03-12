8bbacc611ffab880d530ed8a87b4d387a4298676
betulioo <luis.nunez@bootcamp-upgrade.com>
2024-03-12 10:24:34 +0100

    change

---
+++ index.js
@@ -0,0 +1,23 @@
+var myFun = new Function("5 + 2");
+var shape = "round";
+var size = 1;
+var foo = ["Apple", "Mango", "Orange"];
+var today = new Date();
+
+ // devuelve "function"
+; // devuelve "string"
+; // devuelve "number"
+; // devuelve "object"
+; // devuelve "object"
+typeof doesntExist; // devuelve "undefined"
+
+typeof true; // devuelve "boolean"
+typeof null; // devuelve "object"
+console.log(typeof myFun);
+console.log(typeof shape);
+console.log(typeof size);
+console.log(typeof foo);
+console.log(typeof today);
+console.log(typeof doesntExist);
+console.log(typeof true);
+console.log(typeof null);
